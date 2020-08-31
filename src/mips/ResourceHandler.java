543d010c70bb2eaf8261f369bb8d64cbbec67361
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2020-08-31 17:33:43 -0400

    removed unessisary files

--- src/mips/ResourceHandler.java
+++ src/mips/ResourceHandler.java
@@ -7,23 +7,16 @@ package mips;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.Enumeration;
 import java.util.Objects;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.filechooser.FileSystemView;
 
 /**
@@ -100,7 +93,7 @@ public class ResourceHandler {
                             }
                         }
                         //System.out.println("bruh");
-                        Log.logError(toWrite.getAbsolutePath());
+                        //Log.logError(toWrite.getAbsolutePath());
 
                         InputStream in = new BufferedInputStream(jarFile.getInputStream(entry));
                         OutputStream out = new BufferedOutputStream(new FileOutputStream(toWrite));
