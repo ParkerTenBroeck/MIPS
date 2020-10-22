37f87d3faf071956c579c288fb9e2c7d1e581f36
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2020-10-22 01:27:41 -0400

    added example directory

---
+++ examples/exampleSystemCallPlugin/exampleProgram2.asm
@@ -0,0 +1,9 @@
+main:
+trap 202
+add $4, $2, $0
+trap 200
+trap 203
+add $4, $2, $0
+trap 201
+
+j main
---
+++ examples/exampleSystemCallPlugin/src/examplesystemcallplugin/ExampleSystemCallPlugin.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examplesystemcallplugin;
+
+import org.parker.mips.PluginHandler.SystemCallPluginHandler.SystemCall;
+import org.parker.mips.PluginHandler.SystemCallPluginHandler.SystemCallData;
+import org.parker.mips.PluginHandler.SystemCallPluginHandler.SystemCallPlugin;
+
+/**
+ *
+ * @author parke
+ */
+public class ExampleSystemCallPlugin extends SystemCallPlugin {
+
+    ExampleFrame exampleFrame = new ExampleFrame();
+
+    public ExampleSystemCallPlugin() {
+
+        super(4, "Example_Plugin"); //initiates the plugin with 4 system calls with the name Example_Plugin
+
+        this.addFrameToGUI(exampleFrame);
+
+        SystemCallData[] scd = this.getSystemCallDataFromClass(this.getClass()); //loads the data stored in ExampleSystemCallPlugin
+
+        this.systemCalls[0] = new SystemCall(scd[0], "EXAMPLE_SET_HOURS", this) { //make sure that the name entered here and the name in ExampleSystemCallPlugin match this is for verification
+            @Override
+            public void handleSystemCall() {
+                exampleFrame.opExampleFrame();
+                exampleFrame.setHours(getRegister(4));
+            }
+        };
+        this.systemCalls[1] = new SystemCall(scd[1], "EXAMPLE_SET_MINS", this) {
+            @Override
+            public void handleSystemCall() {
+                exampleFrame.opExampleFrame();
+                exampleFrame.setMins(getRegister(4));
+            }
+        };
+        this.systemCalls[2] = new SystemCall(scd[2], "EXAMPLE_READ_HOURS", this) {
+            @Override
+            public void handleSystemCall() {
+                exampleFrame.opExampleFrame();
+                setRegister(2, exampleFrame.getHours());
+            }
+        };
+        this.systemCalls[3] = new SystemCall(scd[3], "EXAMPLE_READ_MINS", this) {
+            @Override
+            public void handleSystemCall() {
+                exampleFrame.opExampleFrame();
+                setRegister(2, exampleFrame.getMins());
+            }
+        };
+
+    }
+
+    @Override
+    public void init() {
+        //this can be used for any initiation after the constructor if needed
+    }
+
+}
