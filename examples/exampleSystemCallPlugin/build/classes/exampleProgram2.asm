13d666f46ad945f038514768d975150d89cd612b
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2020-12-04 19:12:06 -0500

    updated example plugin.yml

--- examples/exampleSystemCallPlugin/build/classes/exampleProgram2.asm
+++
@@ -1,9 +0,0 @@
-main:
-trap 202
-add $4, $2, $0
-trap 200
-trap 203
-add $4, $2, $0
-trap 201
-
-j main
--- src/main/java/org/parker/mips/plugin/PluginDescription.java
+++ src/main/java/org/parker/mips/plugin/PluginDescription.java
@@ -6,6 +6,7 @@
 package org.parker.mips.plugin;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
 
@@ -15,20 +16,56 @@ import org.yaml.snakeyaml.Yaml;
  */
 public class PluginDescription {
 
+    //required
     public final String NAME;
     public final String MAIN;
     public final String VERSION;
     public final String DESCRIPTION;
+    //not required
+    public final String AUTHOR;
+    public final ArrayList<String> AUTHORS;
+    public final String WEBSITE;
+    public final String PREFIX;
 
     public PluginDescription(Map<String, Object> map) throws InvalidDescriptionException {
-        try{
-        this.NAME = map.get("name").toString();
-        this.MAIN = map.get("main").toString();
-        this.VERSION = map.get("version").toString();
-        this.DESCRIPTION = map.get("description").toString();
-        }catch(Exception e){
+        try {
+            this.NAME = map.get("name").toString();
+            this.MAIN = map.get("main").toString();
+            this.VERSION = map.get("version").toString();
+            this.DESCRIPTION = map.get("description").toString();
+        } catch (Exception e) {
             throw new InvalidDescriptionException("Cannot load base plugin description");
         }
+
+        String temp;
+        try {
+            temp = map.get("author").toString();
+        } catch (Exception e) {
+            temp = null;
+        }
+        this.AUTHOR = temp;
+
+        ArrayList<String> tempA;
+        try {
+            tempA = (ArrayList<String>) map.get("authors");
+        } catch (Exception e) {
+            tempA = null;
+        }
+        this.AUTHORS = tempA;
+
+        try {
+            temp = map.get("website").toString();
+        } catch (Exception e) {
+            temp = null;
+        }
+        this.WEBSITE = temp;
+
+        try {
+            temp = map.get("prefix").toString();
+        } catch (Exception e) {
+            temp = null;
+        }
+        this.PREFIX = temp;
     }
 
     public PluginDescription(InputStream input) throws InvalidDescriptionException {
